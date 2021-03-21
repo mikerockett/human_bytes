1553486f04e331694448d8a7c4d70affe981cf63
Mike Rockett <mrockett@outlook.com>
2021-03-21 16:02:03 +0200

    add editorconfig, format

--- human_bytes.v
+++ human_bytes.v
@@ -1,7 +1,7 @@
 module human_bytes
 
-import math { log, log10, floor, min, pow, is_inf }
-import strconv { format_fl, remove_tail_zeros }
+import math { floor, is_inf, log, log10, min, pow }
+import strconv { format_fl }
 
 const (
 	binary_bit_units  = ['b', 'kibit', 'Mibit', 'Gibit', 'Tibit', 'Pibit', 'Eibit', 'Zibit', 'Yibit']
@@ -31,31 +31,39 @@ pub fn convert(input_val f64) string {
 
 pub fn convert_opt(input_val f64, options Options) string {
 	mut value := input_val
+
 	units := if options.binary {
-		if options.bits { binary_bit_units } else { binary_byte_units }
+		if options.bits { human_bytes.binary_bit_units } else { human_bytes.binary_byte_units }
 	} else {
-		if options.bits { bit_units } else { byte_units }
+		if options.bits { human_bytes.bit_units } else { human_bytes.byte_units }
 	}
+
 	if is_inf(input_val, 0) {
 		return format('', 0, options.precision) + display_unit(units, 0)
 	}
+
 	if options.signed && value == 0 {
 		return format(' ', 0, options.precision) + display_unit(units, 0)
 	}
 	negative := value < 0
+
 	prefix := if negative {
 		'-'
 	} else {
 		if options.signed { '+' } else { '' }
 	}
+
 	if negative {
 		value = -value
 	}
+
 	if value < 1 {
 		return format(prefix, value, options.precision) + display_unit(units, 0)
 	}
+
 	floored := floor(if options.binary { log(value) / log(1024) } else { log10(value) / 3 })
 	exponent := min(floored, units.len - 1)
 	value /= pow(if options.binary { 1024 } else { 1000 }, exponent)
+
 	return format(prefix, value, options.precision) + display_unit(units, exponent)
 }
